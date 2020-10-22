b295e90ad0dfbbaa3e536ee63629d90717336fd6
areye022 <areye022@gmail.com>
2020-10-22 13:06:24 -0700

    added id to each new note/input, when i console, everything works properly, but new inputs not prompting on the screen

--- server.js
+++ server.js
@@ -31,6 +31,26 @@ app.get('/api/notes', function(req,res){
     res.json(notes);
 });
 
+app.post('/api/notes', function(req,res){
+    // creating an id for each note/ activeNote is the object in index.js
+    const newInput= req.body;
+    // if statment to add id to current notes object
+    if (notes.length ===0) {
+        newInput.id = 1;
+    } else {
+        // increase id number by 1 each time theres a new note
+        const newNoteId= notes.length++
+        newInput.id = newNoteId;
+    }
+    // see if newInput working properly
+    console.log(newInput);
+    notes.push(newInput);
+    console.log(notes);
+// writing the new note into the db.json file
+    fs.writeFileSync("./db/db.json", JSON.stringify(notes));
+    res.json(newInput);
+})
+
 
 
 app.listen(PORT, function(){
